ff477b26c1b2673e99102bae64059332559eb13c
user name <user email>
2023-09-29 00:53:53 +0300

    creat models  response on id,  location and all information about user

--- car/users/admin.py
+++ car/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile,Location
+class ProfileAdmin(admin.ModelAdmin):
+    pass
 
-# Register your models here.
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Profile,ProfileAdmin),
+
