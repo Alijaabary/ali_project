2e437ad926092e387104413504fe3eac311ef4ee
user name <user email>
2023-09-29 01:44:30 +0300

    update app car

--- car/car/urls.py
+++ car/car/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for car project.
+URL configuration for project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from main import urls as main_app_urls
+from users import urls as users_app_urls
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(main_app_urls)),
+    path('', include(users_app_urls)),
 ]
+if settings.DEBUG:
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
